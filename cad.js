bf34d4f1a2dfb4b2f3523bab479ee584f625734a
TazioArruda <tazioalmeida@gmail.com>
2023-04-07 01:03:18 -0300

    apiiniciada
    iniciando api

--- cad.js
+++ cad.js
@@ -1,14 +1,36 @@
 const formElement = document.querySelector("#content")
 
+
+let data
+
+async function getApi(){
+    const request = await fetch("http://localhost:3000/paciente")
+    data = await request.json()
+    let textoPaciente = ""
+    data.forEach(element => {
+      textoPaciente = textoPaciente + `<p>nome do paciente ${element.nome}</p>`
+      document.getElementById("teste").innerHTML = textoPaciente
+    });
+
+
+}
+
+getApi()
+const resultadoApi = data
+
+
+
+/*------------------------------------FUNÇÃO DE MUDANÇA DO FORMULARIO ----------------------------*/
+
 function handleNextPage(e) {
   e.preventDefault()
   
   const html = `
   <div id="content1">
 
-            <div id="logocadastro">
+        
                 <img id="logo-form1" src="./img/cadastroformlogo.png"/>
-                 <div id="text1"><p>Seja bem-vindo(a)!</p><p>Escolha uma senha</p></div>
+                 <div id="text1"><p><strong>Seja bem-vindo(a)!</strong></p><p>Escolha uma senha</p></div>
 
                 <form id="singup-form1">
                          <label for="nome" id="senha">Senha</label>
@@ -16,21 +38,23 @@ function handleNextPage(e) {
                          <label for="email" id="confsenha">Confirmar senha</label>
                          <input type="email" id="confirmForm"/><br>
 
-                         <div id="restriForm">
+                         <div id="restriForm1">
                             <div>*Precisa no mínimo 8 digitos<div>
                             <div>*Precisa conter um caractere especial */+.</div>
                             <div>*Precisa conter uma letra em MAIÚSCULA.</div>
-                        <div>
-                         <button type="submit" id="prosseForm">Prosseguir</button>
+                          <div>
+                         
                 </form>
+                <div id="buttom2"><button type="submit" id="prosseForm">Prosseguir</button></div>
 
-            </div>
+
             
     </div>
   `
-
   formElement.innerHTML = html
 }
 
 formElement.addEventListener('submit', handleNextPage)
 
+
+/*-------------------------------------------------FINAL DA FUNÇÃO -------------------------------------------------*/
